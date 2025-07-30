b02e2a39d314a3c35d28890124ed7ee6a3494fd9
samaha74 <mm.samahacs@gmail.com>
2025-07-30 14:36:18 +0300

    Add project files.

---
+++ GradeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GradeChecker
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            int g;
+            while (true)
+            { 
+                Console.Write("Enter your mark (0-100):");
+                g = Convert.ToInt32(Console.ReadLine());
+                if (g >= 0 && g <= 100)
+                    break;
+                Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+            }
+
+            if (g >= 90)
+                Console.WriteLine("Your grade is A");
+            else if (g >= 80)
+                Console.WriteLine("Your grade is B");
+            else if (g >= 70)
+                Console.WriteLine("Your grade is C");
+            else if (g >= 60)
+                Console.WriteLine("Your grade is D");
+            else
+                Console.WriteLine("YOU FAILED");
+
+        }
+    }
+}
---
+++ MultiTable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MultiTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x , i = 1;
+            char c = 'Y';
+            while (c == 'Y')
+            {
+                Console.Write("Enter Your Num: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                while (i < 13)
+                {
+                    Console.WriteLine($"{x} * {i} = {x * i}");
+                    i++;
+                }
+                i = 1;
+                Console.WriteLine("Again?(Y/N) ");
+                c = Convert.ToChar(Console.ReadLine().ToUpper());
+            }
+        }
+    }
+}
